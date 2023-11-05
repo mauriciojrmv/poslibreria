d7094607e5fdcdbad357ffb19f51273c97e57a23
Mauricio Mattinen <mauricio.jr.mattinen.valdez@gmail.com>
2023-11-05 14:33:57 -0400

    first push

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2023 <a href="https://mauriciojrmattinen.wixsite.com/mattinen" target="_blank">Pagina de Mattinen</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
